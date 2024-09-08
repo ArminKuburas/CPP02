2a47f43a42f1c50a493ed6fb6de1fef5496fb980
ArminKuburas <akuburass@gmail.com>
2024-09-08 14:01:08 +0300

    feat: added makefile and main file in exercise03

---
+++ ex03/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 13:59:45 by akuburas          #+#    #+#             */
+/*   Updated: 2024/09/08 14:00:16 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Point.hpp"
+
+bool bsp(Point const a, Point const b, Point const c, Point const point);
+
+int	main(void)
+{
+	Point a(0, 0);
+	Point b(0, 1);
+	Point c(1, 0);
+	Point point(0.5, 0.5);
+
+	if (bsp(a, b, c, point))
+		std::cout << "Point is inside the triangle" << std::endl;
+	else
+		std::cout << "Point is outside the triangle" << std::endl;
+}
